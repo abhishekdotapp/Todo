1a91c1a20c72595f89d8e799961fe42d88db607e
abhishekjaiswal07890 <abhishekjaiswal07890@gmail.com>
2024-07-19 13:22:09 +0000

    checkBox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +12,17 @@ class _HomePageState extends State<HomePage> {
   void check() {
     print('hwllo');
   }
+
+  void checkBoxClicked(bool? value, int index) {
+    setState(() {
+      todolist[index][1] = !todolist[index][1];
+    });
+  }
+
+  List todolist = [
+    ["do dsa problem", true],
+    ["learn flutter", false],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +39,15 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: check,
         child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: todolist.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+              taskname: todolist[index][0],
+              taskComplete: todolist[index][1],
+              onChanged: (value) => checkBoxClicked(value, index));
+        },
       ),
     );
   }
---
+++ lib/utils/todo_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  final String taskname;
+  final bool taskComplete;
+  Function(bool?)? onChanged;
+  TodoTile({
+    super.key,
+    required this.taskname,
+    required this.taskComplete,
+    required this.onChanged,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left:25.0,right:25.0,top:25.0),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Checkbox(value: taskComplete, onChanged: onChanged, activeColor:Colors.black,),
+            Text(taskname),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.blue[400],
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
